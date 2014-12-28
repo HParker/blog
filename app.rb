0b05f1a2697286e5ffecf0ffcd420ff804e2829e
Adam Hess <adamhess1991@gmail.com>
2014-12-27 19:24:14 -0800

    revert to basic hello world

--- app.rb
+++ app.rb
@@ -1,13 +1,18 @@
 require 'sinatra'
 
-get '/' do
-  links = Dir['posts/*.html'].map { |file|
-    basename = File.basename(file, ".*")
-    "<a href='/read/#{basename}'>#{basename}</a>"
-  }
-
-end
-
-get '/read/:filename' do
-  send_file "posts/#{params[:filename]}.html"
+class Blog < Sinatra::Base
+  get '/' do
+    "HELLO THIS IS ADAM"
+=begin
+    links = Dir['posts/*.html'].map { |file|
+      basename = File.basename(file, ".*")
+      "<a href='/read/#{basename}'>#{basename}</a>"
+    }
+=end
+  end
+=begin
+  get '/read/:filename' do
+    send_file "posts/#{params[:filename]}.html"
+  end
+=end
 end
