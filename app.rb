8976773226b10871623048490ef0e5631f3674a5
Adam Hess <adamhess1991@gmail.com>
2014-12-27 22:46:17 -0800

    add unicorn file

--- app.rb
+++ app.rb
@@ -1,18 +1,7 @@
-require 'sinatra'
+require "sinatra/base"
 
 class Blog < Sinatra::Base
   get '/' do
     "HELLO THIS IS ADAM"
-=begin
-    links = Dir['posts/*.html'].map { |file|
-      basename = File.basename(file, ".*")
-      "<a href='/read/#{basename}'>#{basename}</a>"
-    }
-=end
   end
-=begin
-  get '/read/:filename' do
-    send_file "posts/#{params[:filename]}.html"
-  end
-=end
 end
