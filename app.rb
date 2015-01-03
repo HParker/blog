13bfa18d4b2af682af8cfd6ed2ac90398bfcfd7b
Adam Hess <adamhess1991@gmail.com>
2015-01-02 16:57:43 -0800

    add blog posts

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require "sinatra/base"
 
 class Blog < Sinatra::Base
   get '/' do
-    erb :index
+    erb :index, locals: { posts: Dir['posts/**/*.html'].map { |post| { name: File.basename(post, '.html'), time: File.atime(post) } } }
+  end
+  get '/read/:name' do
+    send_file "posts/#{params[:name]}.html"
   end
 end
