8aef8bfae3eecc5b4dc7fdf1b63073977d2d6f7c
Adam Hess <adamhess1991@gmail.com>
2015-07-17 09:25:58 -0600

    add support for sideshows

--- app.rb
+++ app.rb
@@ -6,6 +6,11 @@ class Blog < Sinatra::Base
     erb :index, locals: { posts: Models::Post.all }
   end
 
+  get '/read/reveal.js/*.*' do
+    puts File.expand_path('reveal.js-3.1.0/' + params['splat'].join('.'), settings.public_folder)
+    send_file File.expand_path('reveal.js-3.1.0/' + params['splat'].join('.'), settings.public_folder)
+  end
+
   get '/read/:name' do
     send_file "posts/#{params[:name]}.html"
   end
